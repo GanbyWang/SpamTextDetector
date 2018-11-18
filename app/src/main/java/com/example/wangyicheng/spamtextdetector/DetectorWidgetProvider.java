d72c662546a0082c82d089122daadbb0e3c96fa0
Alex_Wang <wang_yichengpku@163.com>
2018-11-18 15:51:22 -0500

    Refactored the project. Currently a runnable demo

---
+++ app/src/main/java/com/example/wangyicheng/spamtextdetector/DetectorWidgetProvider.java
@@ -0,0 +1,14 @@
+package com.example.wangyicheng.spamtextdetector;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Class definition of the widget
+ * Contains all logic of updating
+ */
+
+public class DetectorWidgetProvider extends AppWidgetProvider {
+
+    // TODO: override functions as needed
+
+}
